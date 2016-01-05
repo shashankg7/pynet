754bd361dae192fc35d7a2c6e5960427d42fe218
mbj111 <mbjoshi111@gmail.com>
2016-01-05 21:15:12 +0530

    test file added to test net.py, test_net.py

---
+++ tests/test_net.py
@@ -0,0 +1,13 @@
+import unittest
+"""
+   Test for the local and the web html page for table table generation
+    
+"""
+class TestNET(unittest.TestCase):
+    
+    def test_net(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
+
